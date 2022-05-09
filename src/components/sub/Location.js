62be0db1bcd07c6b121806794a0fa23db208a842
EuraKo <sow5252@gmail.com>
2022-05-09 10:32:35 +0900

    카카오 토글 맵

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -4,6 +4,7 @@ import { useRef, useEffect, useState } from 'react';
 function Location(props) {
 	// window전역객체에서 kakao라는 이름으로 등록되어있는 객체를 비구조할당으로 직접 변수에 할당
 	const { kakao } = window;
+	console.log(window);
 	const container = useRef(null);
 	const options = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
@@ -11,29 +12,47 @@ function Location(props) {
 	};
 	// useEffect안의 map의 변수를 onClick에서 불러와서 쓰기위해 state로 저장한다
 	const [map, setMap] = useState(null);
+	const [traffic, setTraffic] = useState(false);
 
 	useEffect(() => {
 		console.log(container);
 
 		// container만 쓰면 dom을 가져오지 않아 .current를 불러와야함
-		const map = new kakao.maps.Map(container.current, options);
+		const map_instance = new kakao.maps.Map(container.current, options);
 		// 캄포넌트가 처음 mount시 생성된 인스턴스 map값을 해당 컴포넌트에서 자유롭게 쓰면서 상태관리 하기 위해 map state로 옮겨담음
-		setMap(map);
+		setMap(map_instance);
 	}, []);
 
+	useEffect(() => {
+		console.log(traffic);
+		// 초기에 map이 null로 들어오기 때문에 먼저 있는지 판단을 해야한다.
+		if (map) {
+			traffic
+				? map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+				: map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		}
+	}, [traffic]);
 	return (
 		<Layout name='Location'>
 			<div id='map' ref={container}></div>
-			{/* 버튼 클릭시 map  */}
+			<span>토글 버튼 : </span>
+			<button onClick={() => setTraffic(!traffic)}>
+				{traffic ? 'traffic Off' : 'traffic ON'}
+			</button>
+			{/* 버튼 클릭시 map state에 담겨있는 인스턴스의 addOverlayMap함수 호출 */}
+			<br />
+			<span>따로따로 버튼 : </span>
 			<button
 				onClick={() => {
 					map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+					setTraffic(true);
 				}}>
 				Traffic On
 			</button>
 			<button
 				onClick={() => {
 					map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+					setTraffic(false);
 				}}>
 				Traffix Off
 			</button>
