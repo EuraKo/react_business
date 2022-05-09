456c6ea69f451ce936562ddfc463d70c287be7f3
EuraKo <sow5252@gmail.com>
2022-05-09 15:24:44 +0900

    join ud, email

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,26 +1,60 @@
 import Layout from '../common/Layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// ======= 예시 ===========
+//  es6에서 새로나온 문법 키값을 변수로 치환하고싶을때
+const abc = 'reading';
+const student = {
+	age: 20,
+	[abc]: 'reading',
+};
+console.log(student);
+// ========= 예시 끝 =========
 
 function Join() {
 	const initVal = {
 		userid: '',
+		email: '',
 	};
 	const [val, setVal] = useState(initVal);
+	const [err, setErr] = useState({});
+
+	const check = (val) => {
+		const errs = {};
+		if (val.userid.length < 5) {
+			errs.userid = '아이디를 5글자이상 입력하새요';
+		}
+		if (val.email.length < 3 || !/@/.test(val.email)) {
+			errs.email = '이메일을 3글자이상 @를 포함하여 입력하세요';
+		}
+		return errs;
+	};
 	const handleChange = (e) => {
 		const { name, value } = e.target;
-		console.log(name, value);
+		// console.log(name, value);
 		// setVal({...val, 'userid':value})
+		// 그냥 name을 쓰면 키값은 못가져오므로 []로 가져온다. 상단에 예시 있음
 		setVal({ ...val, [name]: value });
+		console.log(val);
 	};
 
+	const handelSubmit = (e) => {
+		e.preventDefault();
+		setErr(check(val));
+	};
+
+	useEffect(() => {
+		console.log(err);
+	}, [err]);
 	return (
 		<Layout name='Join'>
-			<form action=''>
+			<form onSubmit={handelSubmit}>
 				<fieldset>
 					<legend>회원가입 폼양식</legend>
 					<table border='1'>
 						<caption>회원가입 정보 입력</caption>
 						<tbody>
+							{/* user id */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='userid'>userid</label>
@@ -36,6 +70,23 @@ function Join() {
 									/>
 								</td>
 							</tr>
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>email</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										placeholder='이메일을 입력하세요.'
+										value={val.email}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							{/* button set */}
 							<tr>
 								<th colSpan='2'>
 									<input type='reset' value='cancle' />
