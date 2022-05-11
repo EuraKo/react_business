b9d9b6eaa64e9a7169177316157b5634789808d3
EuraKo <sow5252@gmail.com>
2022-05-11 11:13:15 +0900

    search btn

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -7,15 +7,17 @@ function Flickr() {
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
 
+	const key = 'df93661d16064f006391d9d061379d39';
+	const num = 50;
+	const interest_url = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
+	const search_url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json&tags=ocean`;
+
 	// wrapping 함수에 async키워드 추가
-	const getFlickr = async () => {
-		const method_interest = 'flickr.interestingness.getList';
-		const key = 'df93661d16064f006391d9d061379d39';
-		const num = 500;
-		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
+	const getFlickr = async (method) => {
+		const method_interest = method;
 
 		// 1. async await를 사용해서 데이터를 먼저불러오도록 처리
-		await axios.get(url).then((json) => {
+		await axios.get(method_interest).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
@@ -26,11 +28,25 @@ function Flickr() {
 	};
 
 	useEffect(() => {
-		getFlickr();
+		getFlickr(interest_url);
 	}, []);
 
 	return (
 		<Layout name={'Flickr'} bg={`${path}/img/man1.jpg`}>
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(interest_url);
+				}}>
+				interest gallery
+			</button>
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(search_url);
+				}}>
+				search gallery
+			</button>
 			<div className='frame' ref={frame}>
 				{items.map((item, idx) => {
 					return (
