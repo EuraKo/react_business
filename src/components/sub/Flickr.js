0582103597625d878dc23bdb7f91baed9cb6509c
EuraKo <sow5252@gmail.com>
2022-05-11 11:45:55 +0900

    loading

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,11 +1,17 @@
 import Layout from '../common/Layout';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-const path = process.env.PUBLIC_URL;
+import Masonry from 'react-masonry-component';
 
 function Flickr() {
+	const path = process.env.PUBLIC_URL;
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	const masonryOptions = {
+		transitionDuration: '0.5s',
+	};
 
 	const key = 'df93661d16064f006391d9d061379d39';
 	const num = 50;
@@ -18,13 +24,15 @@ function Flickr() {
 
 		// 1. async await를 사용해서 데이터를 먼저불러오도록 처리
 		await axios.get(method_interest).then((json) => {
-			console.log(json.data.photos.photo);
+			// console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
-
 		// 2. await를 통해 데이터를 다불러온다음 실행
 		console.log('flickr데이터 호출끝! 화면출력 시작');
-		frame.current.classList.add('on');
+		setTimeout(() => {
+			frame.current.classList.add('on');
+			setLoading(false);
+		}, 1000);
 	};
 
 	useEffect(() => {
@@ -33,8 +41,12 @@ function Flickr() {
 
 	return (
 		<Layout name={'Flickr'} bg={`${path}/img/man1.jpg`}>
+			{loading ? (
+				<img className='loading' src={[path + 'img/loading.gif']} />
+			) : null}
 			<button
 				onClick={() => {
+					setLoading(true);
 					frame.current.classList.remove('on');
 					getFlickr(interest_url);
 				}}>
@@ -42,27 +54,30 @@ function Flickr() {
 			</button>
 			<button
 				onClick={() => {
+					setLoading(true);
 					frame.current.classList.remove('on');
 					getFlickr(search_url);
 				}}>
 				search gallery
 			</button>
 			<div className='frame' ref={frame}>
-				{items.map((item, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-										alt=''
-									/>
+				<Masonry elementType={'div'} options={masonryOptions}>
+					{items.map((item, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+											alt=''
+										/>
+									</div>
+									<h2>{item.title}</h2>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
