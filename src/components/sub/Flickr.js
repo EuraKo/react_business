c65e927436fc9d2870a0847ccf46ca3a80d48128
EuraKo <sow5252@gmail.com>
2022-05-11 12:16:32 +0900

    타입정의하여 함수나누기

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -8,22 +8,31 @@ function Flickr() {
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [enableClick, setEnableClick] = useState(true);
 
 	const masonryOptions = {
 		transitionDuration: '0.5s',
 	};
 
-	const key = 'df93661d16064f006391d9d061379d39';
-	const num = 50;
-	const interest_url = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
-	const search_url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json&tags=ocean`;
-
 	// wrapping 함수에 async키워드 추가
-	const getFlickr = async (method) => {
-		const method_interest = method;
+	const getFlickr = async (opt) => {
+		const key = 'df93661d16064f006391d9d061379d39';
+		const num = opt.count;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		let url = '';
 
+		// 인수로 받은 객체의 타입이 interest먄 interest url반환
+		if (opt.type === 'interest') {
+			url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
+		}
+
+		// 인수로 받은 객체의 타입이 search이면 tags값을 받아서 검색어의 데이터를 불러오는 url 반환
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+		}
 		// 1. async await를 사용해서 데이터를 먼저불러오도록 처리
-		await axios.get(method_interest).then((json) => {
+		await axios.get(url).then((json) => {
 			// console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
@@ -32,11 +41,20 @@ function Flickr() {
 		setTimeout(() => {
 			frame.current.classList.add('on');
 			setLoading(false);
+			setEnableClick(true);
 		}, 1000);
 	};
 
 	useEffect(() => {
-		getFlickr(interest_url);
+		getFlickr({
+			type: 'interest',
+			count: 50,
+		});
+		/*  	getFlickr({
+				type: 'search',
+				count: 500,
+        tags:'ocean'
+			}); */
 	}, []);
 
 	return (
@@ -46,17 +64,30 @@ function Flickr() {
 			) : null}
 			<button
 				onClick={() => {
-					setLoading(true);
-					frame.current.classList.remove('on');
-					getFlickr(interest_url);
+					if (enableClick) {
+						setEnableClick(false);
+						setLoading(true);
+						frame.current.classList.remove('on');
+						getFlickr({
+							type: 'interest',
+							count: 50,
+						});
+					}
 				}}>
 				interest gallery
 			</button>
 			<button
 				onClick={() => {
-					setLoading(true);
-					frame.current.classList.remove('on');
-					getFlickr(search_url);
+					if (enableClick) {
+						setEnableClick(false);
+						setLoading(true);
+						frame.current.classList.remove('on');
+						getFlickr({
+							type: 'search',
+							count: 50,
+							tags: 'building',
+						});
+					}
 				}}>
 				search gallery
 			</button>
