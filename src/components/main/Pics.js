f098808ef7422f7a49dca00ea3a7241cc10e9677
EuraKo <sow5252@gmail.com>
2022-05-12 16:18:40 +0900

    동작 속도 변경

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -11,10 +11,25 @@ function Pics(props) {
 		<section id='pics' className='myScroll'>
 			<h2
 				style={
-					position >= 0 ? { transform: `translateX(${position}px)` } : null
+					// 보정된 현재 섹션의 위치값에 도달해야지 스타일값 연동
+					position >= 0
+						? { transform: `translateX(${position / 1.2}px)` }
+						: null
 				}>
 				flickr
 			</h2>
+			<p
+				style={
+					position >= 0
+						? {
+								transform: `translateX(${position * 1.2}px) scale(${
+									1 + position / 1000
+								})`,
+						  }
+						: null
+				}>
+				flickr
+			</p>
 		</section>
 	);
 }
