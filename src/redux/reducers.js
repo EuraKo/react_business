1f3576f1d9780ac7d6f10b02a9756195aa763a65
EuraKo <sow5252@gmail.com>
2022-05-16 10:28:41 +0900

    step1 - redux saga reducer action type세분화

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -9,15 +9,7 @@ const memberReducer = (state = { members: [] }, action) => {
 			return state;
 	}
 };
-const galleryReducer = (state = { gallery: [] }, action) => {
-	switch (action.type) {
-		case 'SET_GALLERY':
-			return { ...state, gallery: action.payload };
 
-		default:
-			return state;
-	}
-};
 // reducer의 state인수값은 항상 객체가 들어와야한다 그래서 배열도 객체로 감싼다. youtube 경우 불특정한 객체가 들어와야하므로 빈배열로 넣어준다.
 const youtubeReducer = (state = { youtube: [] }, action) => {
 	switch (action.type) {
@@ -28,7 +20,30 @@ const youtubeReducer = (state = { youtube: [] }, action) => {
 			return state;
 	}
 };
+const galleryReducer = (state = { gallery: [] }, action) => {
+	switch (action.type) {
+		case 'SET_GALLERY':
+			return { ...state, gallery: action.payload };
 
+		default:
+			return state;
+	}
+};
+const flickrReducer = (state = { flickr: [] }, action) => {
+	switch (action.type) {
+		case 'FLICKR_START':
+			return { ...state };
+
+		case 'FLICKR_SUCCESS':
+			return { ...state, flickr: action.payload };
+
+		case 'FLICKR_ERROR':
+			return { ...state, error: action.payload };
+
+		default:
+			return state;
+	}
+};
 /* 
 만약 값이 안들어 왔을 경우 미리 값을 지정. 최신 문법임
 function plus(num1 = 1, num2 = 3) {
@@ -40,6 +55,7 @@ const reducers = combineReducers({
 	memberReducer,
 	youtubeReducer,
 	galleryReducer,
+	flickrReducer,
 });
 
 export default reducers;
