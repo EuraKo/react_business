a98fe05a33e61b003e9cddc6229058cafd635fa5
EuraKo <sow5252@gmail.com>
2022-05-16 10:37:37 +0900

    스토어에 데이터 저장시 리듀서에 사가 미들웨어 적용

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,18 @@
-import { createStore } from 'redux';
+// 미들웨어 추가기능 모듈 import
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+// saga middleware import
+import createSagaMiddleware from '@redux-saga/core';
+// 미들웨어로 적용할 saga파일 import
+import rootSaga from './saga';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+// const store = createStore(reducers);
+// 스토어 생성시 리듀서에 saga미들웨어 적용 후 저장
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+// store에 적용된 sagaMiddleware를 통해 rootSaga기능 활성화
+sagaMiddleware.run(rootSaga);
+
 export default store;
